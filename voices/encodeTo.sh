5e68dd1a45a635b4e7eed770610438387ef581cb
Johnny Jazeix <jazeix@gmail.com>
2022-11-23 14:33:51 +0100

    encodeTo.sh, parallelise encoding

--- voices/encodeTo.sh
+++ voices/encodeTo.sh
@@ -59,19 +59,29 @@ else
     exit 1
 fi
 
-echo "Transcode ogg files to $format"
-for f in $(find . -type f -name \*.ogg)
-do
-    #echo "Processing $f"
-    $encoder -v warning -i $f -acodec $codec ${f%.*}.${format}
+task() {
+    echo "Processing $1"
+    $encoder -v warning -i $1 -acodec $codec ${1%.*}.${format} </dev/null > /dev/null 2>&1
     if [ $? -ne 0 ]
     then
        echo "ERROR: Failed to convert $f"
     fi
-    rm -f $f
+    rm -f $1
+}
+
+parallelCount=4
+echo "Transcode ogg files to $format"
+start=$SECONDS
+for f in $(find . -type f -name \*.ogg)
+do
+    ((i=i%parallelCount)); ((i++==0)) && wait
+    task $f &
 done
+duration=$(( SECONDS - start ))
+echo "Conversion took $duration seconds"
 
 echo "Fix symlinks"
+start=$SECONDS
 for f in $(find . -type l -name \*.ogg)
 do
     #echo "Processing $f"
@@ -79,3 +89,6 @@ do
     rm $f
     ln -s -r ${target%.*}.${format} ${f%.*}.${format}
 done
+duration=$(( SECONDS - start ))
+echo "Fix symlinks took $duration seconds"
+
